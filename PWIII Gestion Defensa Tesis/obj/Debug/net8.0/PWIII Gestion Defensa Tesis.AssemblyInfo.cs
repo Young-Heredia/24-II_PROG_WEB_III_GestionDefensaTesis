bb619d5a3a6293e5bb8f2b63ceb83a56ba99042f
Young-Heredia <hse1010674@est.univalle.edu>
2024-11-22 12:18:52 -0400

    Versión 3 - StudentsController

--- PWIII Gestion Defensa Tesis/obj/Debug/net8.0/PWIII Gestion Defensa Tesis.AssemblyInfo.cs
+++ PWIII Gestion Defensa Tesis/obj/Debug/net8.0/PWIII Gestion Defensa Tesis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PWIII Gestion Defensa Tesis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac357546b63461dfd2155b5577ffd5c3df66ad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PWIII Gestion Defensa Tesis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PWIII Gestion Defensa Tesis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
